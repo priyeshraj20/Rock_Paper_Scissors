b7c15e1a43a87e51c0d3b19fd66809dd12f97ae2
priyeshraj20 <priyeshraj20@gmail.com>
2024-10-04 21:55:02 +0530

    Added function to generate the computer choises

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("Hello world!");
+let humanScore = 0;
+let computerScore = 0;
+
+
+//computer choice
+function getComputerChoise() {
+    const n = Math.floor(Math.random() * 3);
+    if(n===0) {
+        return "rock";
+    }
+
+    else if (n===1){
+         return "paper";
+    }
+
+    else {
+        return "scissor";
+    }
+}
+
+
+console.log(getComputerChoise());
