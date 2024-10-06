9890a85be62de004c309b9e618c8d545664b6c71
priyeshraj20 <priyeshraj20@gmail.com>
2024-10-06 23:01:15 +0530

    Basic logic for the game

--- script.js
+++ script.js
@@ -19,4 +19,63 @@ function getComputerChoise() {
 }
 
 
-console.log(getComputerChoise());
+//get human choises
+
+function getHumanChoise() {
+    const humanInput = window.prompt("Enter your choise ");
+    const humanChoise = humanInput.toLowerCase();
+    return humanChoise;
+}
+
+//Playing a round 
+
+function playRound (compChoise, humChoise) {
+    if(compChoise === humChoise) {
+         return "Draw";
+        
+    }
+    else if(compChoise === "rock") {
+
+        if (humChoise === "paper"){
+            humanScore++;
+            return "Player Wins"; 
+        }
+
+        else {
+            computerScore++;
+            return "Computer Wins"; 
+        }
+    }
+
+    else if(compChoise === "paper") {
+
+        if(humChoise === "scissor") {
+            humanScore++;
+            return "Player Wins"; 
+        }
+        else {
+            return "Computer Wins";
+            computerScore++;
+            
+        }
+    }
+    else if(compChoise === "scissor") {
+        if(humChoise === "rock") {
+            humanScore++;
+             return "Player Wins"; 
+        }  
+        else {
+            computerScore++;
+            return "Computer Wins";  
+        }  
+
+    }
+}
+
+const ch1 = getComputerChoise();
+const ch2 = getHumanChoise();
+console.log(ch1);
+console.log(ch2);
+
+console.log(playRound(ch1, ch2));
+
